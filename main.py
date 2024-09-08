16a14b354ca4a861236c6642660daa1edeb2f241
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-09-08 16:06:00 +0500

    mod main.py

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-from flask import Flask, render_template
-
-
-app = Flask(__name__)
-
-@app.route("/")
-@app.route("/home/")
-def home():
-    context = {
-        "page": "home",
-    }
-    return render_template("home.html", **context)
-
-@app.route("/blog/")
-def blog():
-    context = {
-        "page": "blog"
-    }
-    return render_template("blog.html",  **context)
-
-@app.route("/about/")
-def about():
-    context = {
-        "page": "about"
-    }
-    return render_template("about.html",  **context)
+from app import app
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
